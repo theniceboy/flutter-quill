9a6ad36d7c0623d3eda1ac4f9288e95381673f5d
David Chen <darkwei233@gmail.com>
2025-05-26 00:16:28 -0700

    fix flutter 3.32 macos crash

--- lib/src/editor/raw_editor/raw_editor_state_text_input_client_mixin.dart
+++ lib/src/editor/raw_editor/raw_editor_state_text_input_client_mixin.dart
@@ -91,6 +91,7 @@ mixin RawEditorStateTextInputClientMixin on EditorState
           allowedMimeTypes: widget.config.contentInsertionConfiguration == null
               ? const <String>[]
               : widget.config.contentInsertionConfiguration!.allowedMimeTypes,
+          viewId: View.of(context).viewId,
         ),
       );
 
